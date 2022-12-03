39bbc7fa79422bc49a709ebb6798e75243f0fcb3
cornchiplick <ilsunkiss@gmail.com>
2022-12-04 00:37:26 +0900

    Add: Transactional 처리

--- dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,18 +1,23 @@
 package com.fastcampus.programming.dmaker.controller;
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.service.DMakerService;
+import jakarta.validation.Valid;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class DMakerController {
+
   private final DMakerService dMakerService;
 
   @GetMapping("/developers")
@@ -23,12 +28,14 @@ public class DMakerController {
     return Arrays.asList("snow", "Elsa", "Olaf");
   }
 
-  @GetMapping("/create-developer")
-  public List<String> createDevelopers() {
+  @PostMapping("/create-developer")
+  public List<String> createDevelopers(
+      @Valid @RequestBody CreateDeveloper.Request request
+  ) {
     // GET /developers HTTP/1.1
-    log.info("GET /create-developer HTTP/1.1");
+    log.info("request : {}", request);
 
-    dMakerService.createDeveloper();
+    dMakerService.createDeveloper(request);
 
     return Collections.singletonList("Olaf");
   }
--- dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -1,27 +1,34 @@
 package com.fastcampus.programming.dmaker.service;
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper.Request;
 import com.fastcampus.programming.dmaker.entity.Developer;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
+import jakarta.persistence.EntityManager;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class DMakerService {
   private final DeveloperRepository developerRepository;
+  private final EntityManager em;
 
   @Transactional
-  public void createDeveloper() {
+  public void createDeveloper(CreateDeveloper.Request request) {
+    // business logic start
     Developer developer = Developer.builder()
-        .developerLevel(DeveloperLevel.JUNGNIOR)
-        .developerSkillType(DeveloperSkillType.FRONT_END)
-        .experienceYears(2)
-        .name("Olaf")
-        .age(5)
-        .build();
+      .developerLevel(DeveloperLevel.JUNGNIOR)
+      .developerSkillType(DeveloperSkillType.FRONT_END)
+      .experienceYears(2)
+      .name("Olaf")
+      .age(5)
+      .build();
 
     developerRepository.save(developer);
   }
